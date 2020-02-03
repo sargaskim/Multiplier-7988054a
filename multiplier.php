5d80843f065aeaceb9c01e9c3c5171257fbf1dce
sargaskim <2103702@talnet.nl>
2020-02-03 10:05:01 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$computter1 = 'Ik ga twee getallen met elkaar vermenigvuldigen'.PHP_EOL;
+$computter2 = 'Eerste getal?'.PHP_EOL;
+echo $computter1, $computter2;
+
+$getal1 = readline("");
+$getal2 = readline("");
+
+echo 'Uw resultaat is: '. $getal1*$getal2;
